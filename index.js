0be4bf92b54e0e135c2e494f98bfd52d8aaafa80
Atharva <atharva.chavan907@gmail.com>
2025-12-27 19:59:05 +0530

    added /github

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express()
 
 const port = 4000
 
+const githubData = {
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -16,6 +22,11 @@ app.get('/login', (req, res) => {
 app.get('/youtube', (req, res) => {
     res.send('<h1>Chai aur code</h1>')
 })
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
